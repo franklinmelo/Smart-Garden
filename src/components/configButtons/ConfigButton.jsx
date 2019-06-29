ce6557de388ddb2d8552a733ac1fa289740909a5
franklinmelo <franklinmelo18@gmail.com>
2019-06-29 14:59:10 -0300

    feat(ConfigView): implemented mock structure of config page

---
+++ src/components/configButtons/ConfigButton.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import { withStyles, ThemeProvider } from '@material-ui/styles';
+import styles from './styles';
+import theme from '../../Theme';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import RemoveIcon from '@material-ui/icons/Remove';
+import AddIcon from '@material-ui/icons/Add';
+import Button from '@material-ui/core/Button';
+
+function ConfigButton({
+  classes,
+  title,
+  value,
+  sufix,
+  incrementValue,
+  decrementValue
+}) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Grid
+        item
+        container
+        direction="row"
+        justify="center"
+        className={classes.marginVertical}
+      >
+        <Grid item xs={4} sm={2} container alignItems="center" justify="center">
+          <Typography variant="button" color="primary">
+            {title}:
+          </Typography>
+        </Grid>
+        <Grid
+          item
+          xs={7}
+          sm={3}
+          container
+          direction="row"
+          justify="space-evenly"
+          alignItems="center"
+          style={{
+            borderRadius: '5px',
+            backgroundColor: '#6d6e70'
+          }}
+        >
+          <Grid item xs={4}>
+            <Button
+              color="primary"
+              style={{ width: '100%' }}
+              onClick={e => decrementValue(title)}
+            >
+              <RemoveIcon />
+            </Button>
+          </Grid>
+          <Grid item xs={4}>
+            <Typography>
+              {value} <Typography variant="caption">{sufix}</Typography>
+            </Typography>
+          </Grid>
+          <Grid item xs={4}>
+            <Button
+              color="primary"
+              style={{ width: '100%' }}
+              name="teste"
+              onClick={e => incrementValue(title)}
+            >
+              <AddIcon />
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
+  );
+}
+
+export default withStyles(styles)(ConfigButton);
--- src/pages/configView/ConfigView.jsx
+++ src/pages/configView/ConfigView.jsx
@@ -1,58 +1,117 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { withStyles, ThemeProvider } from '@material-ui/styles';
 import theme from '../../Theme';
 import styles from './styles';
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import MenuList from '@material-ui/core/MenuList';
-import MenuItem from '@material-ui/core/MenuItem';
 import Typography from '@material-ui/core/Typography';
-import { Switch } from '@material-ui/core';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Button from '@material-ui/core/Button';
+import ConfigButton from '../../components/configButtons/ConfigButton';
 
 function ConfigView({ classes }) {
-  const [operationState, setOperationState] = useState({
-    operationMode: true
-  });
+  const [flowTime, setFlowTime] = useState(0);
+  const [vases, setVases] = useState([]);
+  const vasesArray = [
+    { name: 'Vaso1', value: 45 },
+    { name: 'Vaso2', value: 30 },
+    { name: 'Vaso3', value: 10 },
+    { name: 'Vaso4', value: 10 }
+  ];
 
-  const handleChange = name => event => {
-    setOperationState({ ...operationState, [name]: event.target.checked });
-  };
+  useEffect(() => {
+    if (vases.length === 0) setVases(vasesArray);
+  }, [vases, vasesArray]);
 
+  function incrementValueVases(name) {
+    const mock = vases.map(data => {
+      if (data.name === name) {
+        return { ...data, value: data.value + 1 };
+      } else {
+        return data;
+      }
+    });
+    setVases(mock);
+  }
+
+  function decrementValueVases(name) {
+    const mock = vases.map(data => {
+      if (data.name === name) {
+        return { ...data, value: data.value === 0 ? 0 : data.value - 1 };
+      } else {
+        return data;
+      }
+    });
+    setVases(mock);
+  }
+
+  function incrementFlowTime() {
+    setFlowTime(flowTime + 1);
+  }
+  function decrementFlowTime() {
+    if (flowTime === 0) {
+      setFlowTime(0);
+    } else {
+      setFlowTime(flowTime - 1);
+    }
+  }
   return (
     <ThemeProvider theme={theme}>
-      <Grid
-        container
-        direction="column"
-        justify="center"
-        style={{ height: '100vh' }}
-      >
-        <Grid item container direction="row" justify="center">
-          <Paper className={classes.paper}>
-            <MenuList>
-              <MenuItem>
-                <Typography variant="inherit" noWrap>
-                  Modo de operção Manual/Automatico
-                </Typography>
-                <Switch
-                  checked={operationState.operationMode}
-                  onChange={handleChange('operationMode')}
-                  value="operationMode"
-                  inputProps={{ 'aria-label': 'teste' }}
-                  color="primary"
-                />
-              </MenuItem>
-              <MenuItem>
-                <Typography variant="inherit">
-                  A very long text that overflows
-                </Typography>
-              </MenuItem>
-              <MenuItem>
-                <Typography variant="inherit" noWrap>
-                  A very long text that overflows
-                </Typography>
-              </MenuItem>
-            </MenuList>
-          </Paper>
+      <Grid container direction="column">
+        <Grid
+          item
+          container
+          direction="column"
+          justify="center"
+          className={classes.marginVertical}
+        >
+          <Typography variant="h4" color="secondary">
+            Modo de operação
+          </Typography>
+          <Grid item className={classes.marginVertical}>
+            <ButtonGroup color="primary" size="large">
+              <Button>Manual</Button>
+              <Button>Automatico</Button>
+            </ButtonGroup>
+          </Grid>
+        </Grid>
+        <Grid
+          item
+          container
+          direction="column"
+          justify="center"
+          className={classes.marginVertical}
+        >
+          <Typography variant="h4" color="secondary">
+            Modo Automatico
+          </Typography>
+          <ConfigButton
+            title="tempo de vazão"
+            value={flowTime}
+            sufix="seg"
+            incrementValue={incrementFlowTime}
+            decrementValue={decrementFlowTime}
+          />
+        </Grid>
+        <Grid
+          item
+          container
+          direction="column"
+          justify="center"
+          className={classes.marginVertical}
+        >
+          <Typography variant="h5" color="primary">
+            Umidade Ideal:
+          </Typography>
+          {vases.map(data => (
+            <ConfigButton
+              key={data.name}
+              title={data.name}
+              value={data.value}
+              incrementValue={incrementValueVases}
+              decrementValue={decrementValueVases}
+              sufix="%"
+            />
+          ))}
         </Grid>
       </Grid>
     </ThemeProvider>
